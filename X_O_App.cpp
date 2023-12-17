dd78a7a6c2e0b630c72b7cd221d8fa62a405b810
IsraaMohamed <122988145+IsraaXx@users.noreply.github.com>
2023-12-17 20:38:56 +0200

    Update X_O_App.cpp

--- X_O_App.cpp
+++ X_O_App.cpp
@@ -1,6 +1,6 @@
 // Class definition for XO_App class
 // Author:  Mohammad El-Ramly
-// Date:    10/10/2022
+// Date:    10/10/2022.
 // Version: 1
 #include <iostream>
 #include"../include/BoardGame_Classes.hpp"
