315389c09ef20f48db3327b0fbe7029e58f3e989
Eng-AmanyMohamed <147101273+Eng-AmanyMohamed@users.noreply.github.com>
2023-12-21 23:33:44 +0200

    Update X_O_App.cpp

--- X_O_App.cpp
+++ X_O_App.cpp
@@ -6,48 +6,84 @@
 #include"../include/BoardGame_Classes.hpp"
 #include "XO_25_Board.h"
 #include "Pyramic_Tic_Tac_Toe.h"
+#include "Connect4.h"
 using namespace std;
 int main(){
 int choice;
     Player *players[2];
     players[0] = new Player(1, 'x');
-    cout << "Welcome to FCAI X-O Game. :)\n";
-    cout << "Menu of games :\n1- XO Board\n2- XO 5*5 Board\n3-Pyramic_Tic_Tac_Toe\n4-Four-in-a-row\n";
+    cout << "Welcome to FCAI X-O Game. :\n";
+    cout << "Menu of games :\n1- XO Board\n2- XO 5*5 Board\n3- Pyramic_Tic_Tac_Toe\n4- Four-in-a-row \n";
     cout << "Enter the number of the game you want to play :\n";
     int num;
     cin >> num;
     if (num == 1) {
-    cout << "Press 1 if you want to play with computer: ";
+    cout << "1- Play with computer\n2- play with another player \n";
+    cout << "Enter the number that you want to play with :\n";
     cin >> choice;
-    if (choice != 1)
-        players[1] = new Player(2, 'o');
-    else
-        //Player pointer points to child
-        players[1] = new RandomPlayer('o', 3);
+    if (choice == 1){
+       players[1] = new RandomPlayer('o', 3);
         GameManager x_o_game(new X_O_Board(), players);
-        x_o_game.run();
+        x_o_game.run();}
+        
+    else if(choice == 2){
+        players[1] = new Player(2, 'o');
+        GameManager x_o_game(new X_O_Board(), players);
+        x_o_game.run();}
     } 
     else if (num == 2) {
-        cout << "Press 1 if you want to play with computer: ";
+        cout << "1- Play with computer\n2- play with another player\n ";
+         cout << "Enter the number that you want to play with :\n";
        cin >> choice;
-       if (choice != 1)
-        players[1] = new Player(2, 'o');
-    else
-        //Player pointer points to child
-        players[1] = new RandomPlayer('o', 12);
+       if (choice == 1){
+       players[1] = new RandomPlayer('o', 12);
         GameManager x_o_game(new XO_25_Board(), players);
         x_o_game.run();
+       } 
+    else if(choice == 2){
+        players[1] = new Player(2, 'o');
+        GameManager x_o_game(new XO_25_Board(), players);
+        x_o_game.run();}
     }
     else if (num == 3) {
-    cout << "Press 1 if you want to play with computer: ";
+    cout << "1- Play with computer\n2- play with another player\n3- AI Player\n ";
+     cout << "Enter the number that you want to play with :\n";
     cin >> choice;
-    if (choice != 1)
-        players[1] = new Player(2, 'o');
-    else
-        //Player pointer points to child
-         players[1] = new RandomPlayerForPyramid('o', 3 , 5);
+    if(choice == 1)
+    {
+        players[1] = new RandomPlayerForPyramid('o', 3 ,5);
         GameManager x_o_game(new Pyramic_Tic_Tac_Toe(), players);
         x_o_game.run();
     }
+    else if (choice == 2){
+        players[1] = new Player(2, 'o');
+        GameManager x_o_game(new Pyramic_Tic_Tac_Toe(), players);
+        x_o_game.run();
+        }
+    
+    else if(choice == 3){
+       players[1] = new AIComputerPlayer('o',new Pyramic_Tic_Tac_Toe());
+       GameManager x_o_game(new Pyramic_Tic_Tac_Toe(), players);
+        x_o_game.run();
+    }
+    }
+    else if(num==4){
+        cout << "1-Play with computer\n2-play with another player \n ";
+         cout << "Enter the number that you want to play with :\n";
+        cin >> choice;
+        if (choice == 1){
+        players[1] = new RandomPlayer('o', 3);
+        GameManager x_o_game(new Connect4_Board() , players);
+        x_o_game.run();
+        }
+            
+        else if(choice == 2){
+            players[1] = new Player(2, 'o');
+            GameManager x_o_game(new Connect4_Board() , players);
+           x_o_game.run();
+    }
+    }
     system("pause");
+
+  
 }
